fa92b021a834cbae36c4324918120d4e19f11f2c
Adam Brown <adamarthurryan@gmail.com>
2026-01-08 14:24:47 -0400

    various

--- refactored/common.py
+++ refactored/common.py
@@ -36,11 +36,11 @@ def is_between(series, range):
     (low,high) = range
     return (series > low) & (series < high)
 
-def plot_proper_motion(table, decorator='ko') :
+def plot_proper_motion(table, decorator='ko', markersize=0.1, alpha=0.5) :
     """Plot propermotion in GD1 coordinates from given dataframe"""
     x = table['pm_phi1']
     y = table['pm_phi2']
-    plt.plot(x, y, decorator, markersize=0.1, alpha=.5)
+    plt.plot(x, y, decorator, markersize=markersize, alpha=alpha)
 
         
     plt.xlabel('Proper motion phi1 (mas/yr GD1 frame)')
@@ -49,20 +49,58 @@ def plot_proper_motion(table, decorator='ko') :
     plt.xlim(-12, 8)
     plt.ylim(-10, 10);
 
+    #plt.axis('equal');
+
     return plt
 
-def plot_cmd(table):
+def plot_ra_dec(table, decorator='ko', markersize=0.1, alpha=0.5) :
+    """Plot location in ra/dec coordinates from given dataframe"""
+    x = table['ra']
+    y = table['dec']
+    plt.plot(x, y, decorator, markersize=markersize, alpha=alpha)
+
+
+    plt.xlabel('RA (deg)')
+    plt.ylabel('Dec (deg)');
+
+    plt.axis('equal');
+
+    return plt
+
+def plot_gd1(table, decorator='ko', markersize=0.1, alpha=0.5, limits=None) :
+    """Plot location in ra/dec coordinates from given dataframe"""
+    x = table['phi1']
+    y = table['phi2']
+    plt.plot(x, y, decorator, markersize=markersize, alpha=alpha)
+
+
+
+    plt.xlabel('phi1 GD-1 frame (deg)')
+    plt.ylabel('phi2 GD-1 frame (deg)');
+
+    plt.axis('equal');
+
+    if limits:
+        plt.xlim(limits[0])
+        plt.ylim(limits[1])
+    
+
+    return plt
+
+def plot_cmd(table, decorator='ko', markersize=0.1, alpha=0.5):
     y=table['g_mean_psf_mag']
     x=table['g_mean_psf_mag'] - table['i_mean_psf_mag']
 
-    plt.plot(x,y,'ko', markersize=.1, alpha=.5)
-    
+    plt.plot(x,y, decorator, markersize=markersize, alpha=alpha)
+
     plt.xlim(0,3)
     plt.ylim(14,22)
     plt.gca().invert_yaxis()
     
     plt.ylabel('Magnitude $(g)$')
     plt.xlabel('Color $(g-i)$')
+
+    return plt
 
 def download(url):
     filename = basename(url)
